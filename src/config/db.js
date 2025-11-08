dfe19432f69ecd94ea6b7fe0916ca34dc96c4b37
Apoorv Rathore <0211ite094@niet.co.in>
2025-11-08 12:51:52 +0530

    Modify db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Mongo DB Connected");
+        const connectionInstace = await mongoose.connect(`${process.env.MONGO_URL}/${process.env.DB_NAME}`);
+        console.log(`MONGO_DB CONNECTED !! DB_HOST: ${connectionInstace.connection.host}`);
+        console.log(`DB NAME: ${process.env.DB_NAME}`);
     }catch(err){
         console.log("Mongo db connection error: "+err.message);
     }
